42ce037d3dfc226bddaf724f0286774de9144f1c
Boast-a-lot <xuxicheng20@mails.ucas.ac.cn>
2024-11-12 18:24:27 +0800

    全部文件

---
+++ exp17/mycpu_env/module_verify/tlb_verify/rtl/xilinx_ip/clk_pll_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Mon Nov 11 19:24:28 2024
+-- Host        : LAPTOP-476JT8H0 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               D:/aaa/UCAS_CAlab_19/exp17/mycpu_env/module_verify/tlb_verify/rtl/xilinx_ip/clk_pll_stub.vhdl
+-- Design      : clk_pll
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tfbg676-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_pll is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_pll;
+
+architecture stub of clk_pll is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
